975b2cf263a11ad368a19147f196bef20e3ba7b2
Daniël de Kok <me@danieldk.eu>
2012-07-17 16:58:04 +0200

    Cleanup imports.

--- flake.hs
+++ flake.hs
@@ -1,32 +1,20 @@
 module Main where
 
+import           Control.Monad (forever, when)
+import           Data.Bits (shiftL, xor)
+import           Data.ByteString.Char8 (pack)
+import           Data.List (genericIndex)
+import qualified Data.Unique as U
+import           Data.Word (Word64)
+import           Foreign.C.Types (CInt(..), CLong)
+import           Foreign.Marshal.Utils (with)
+import           Foreign.Ptr (Ptr, castPtr, nullPtr)
+import           Foreign.Storable (Storable(..))
+import qualified Network.Info as NI
+import           System.Environment (getArgs)
+import           System.Exit (exitFailure)
+import           System.IO (hPutStrLn, stderr)
 import qualified System.ZMQ3 as ZMQ
-import qualified Data.ByteString as SB
-import Control.Monad
-import Data.ByteString.Char8 (pack, unpack)
-import Control.Concurrent (threadDelay)
-import Data.Time.Clock
-import System.Time
-import Data.Time.Clock.POSIX
-import Data.Bits
-import Network.Info
-import GHC.Word
-import Numeric
-import Data.Char
-import Data.List
-
-import System.IO
-import System.Exit
-import System.Environment
-
-import Foreign
-import Foreign.C
-
-import Data.IORef
-import System.IO.Unsafe
-import Data.Unique
-
-import Control.Concurrent (threadDelay, forkIO)
 
 type MicroTime = Word64
 getMicroTime :: IO MicroTime
@@ -66,16 +54,16 @@ intToBase62 n = go n ""
                 chars62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-macToInt :: MAC -> Int
-macToInt (MAC a b c d e f) = foldl (\acc x -> (shiftL acc 8) `xor` (fromIntegral x :: Int)) 0 [a, b, c, d, e, f]   
+macToInt :: NI.MAC -> Int
+macToInt (NI.MAC a b c d e f) = foldl (\acc x -> (shiftL acc 8) `xor` (fromIntegral x :: Int)) 0 [a, b, c, d, e, f]   
 
 
-genId :: MicroTime -> MAC -> Unique -> String
+genId :: MicroTime -> NI.MAC -> U.Unique -> String
 genId t m u =
     let timestamp = toInteger t
         shifted  = timestamp `shiftL` 32
         mac = toInteger $ (macToInt m) `shiftL` 16
-        rruid = toInteger $ hashUnique u `mod` (2 `shiftL` 16)
+        rruid = toInteger $ U.hashUnique u `mod` (2 `shiftL` 16)
         flake = shifted + mac + rruid
     in intToBase62 flake
 
@@ -86,8 +74,8 @@ main = do
         hPutStrLn stderr "usage: flake <ethernet_device>"
         exitFailure
     let ethernetDevice = args !! 0
-    interfaces <- getNetworkInterfaces
-    let m = mac $ head $ filter (\a -> name a == ethernetDevice) interfaces
+    interfaces <- NI.getNetworkInterfaces
+    let m = NI.mac $ head $ filter (\a -> NI.name a == ethernetDevice) interfaces
 
     ZMQ.withContext 1 $ \c ->
         ZMQ.withSocket c ZMQ.Rep $ \s -> do
@@ -97,6 +85,6 @@ main = do
     loop m s = forever $ do
         msg <- ZMQ.receive s
         currentTime <- getMicroTime
-        unique <- newUnique
+        unique <- U.newUnique
         let flake = genId currentTime m unique 
         ZMQ.send s [] $ pack flake

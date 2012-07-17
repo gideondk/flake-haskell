2dc58327b6a1115c4c61d5dff41eeb3b3a0f820f
Daniël de Kok <me@danieldk.eu>
2012-07-17 17:28:13 +0200

    Use 'time' module for getting miliseconds since epoch.

--- flake.hs
+++ flake.hs
@@ -6,43 +6,17 @@ import           Control.Monad (forever, when)
 import           Data.Bits (shiftL, xor)
 import qualified Data.ByteString as B
 import           Data.ByteString.Char8 ()
+import qualified Data.Time.Clock.POSIX as CP
 import qualified Data.Unique as U
-import           Data.Word (Word64)
-import           Foreign.C.Types (CInt(..), CLong)
-import           Foreign.Marshal.Utils (with)
-import           Foreign.Ptr (Ptr, castPtr, nullPtr)
-import           Foreign.Storable (Storable(..))
 import qualified Network.Info as NI
 import           System.Environment (getArgs)
 import           System.Exit (exitFailure)
 import           System.IO (hPutStrLn, stderr)
 import qualified System.ZMQ3 as ZMQ
 
-type MicroTime = Word64
-getMicroTime :: IO MicroTime
-
-data CTimeval = MkCTimeval !CLong !CLong
-
-instance Storable CTimeval where
-    sizeOf _    = (sizeOf (undefined :: CLong)) * 2
-    alignment _ = alignment (undefined :: CLong)
-    peek p = do
-        s   <- peekElemOff (castPtr p) 0
-        mus <- peekElemOff (castPtr p) 1
-        return (MkCTimeval s mus)
-    poke p (MkCTimeval s mus) = do
-        pokeElemOff (castPtr p) 0 s
-        pokeElemOff (castPtr p) 1 mus
-
-foreign import ccall unsafe "time.h gettimeofday" gettimeofday :: Ptr CTimeval -> Ptr () -> IO CInt
-
--- | Get the current POSIX time from the system clock.
-getMicroTime = with (MkCTimeval 0 0) $ \ptval -> do
-    result <- gettimeofday ptval nullPtr
-    if (result == 0)
-        then f `fmap` peek ptval
-        else fail ("error in gettimeofday: " ++ (show result))
-    where f (MkCTimeval a b) = fromIntegral a * 1000000 + fromIntegral b
+msSinceEpoch :: Integral t => IO t
+msSinceEpoch =
+  (floor . (1000 *)) `fmap` CP.getPOSIXTime
 
 intToBase62 :: Integer -> B.ByteString
 intToBase62 =
@@ -58,10 +32,9 @@ macToInt :: NI.MAC -> Int
 macToInt (NI.MAC a b c d e f) = foldl (\acc x -> (shiftL acc 8) `xor` (fromIntegral x :: Int)) 0 [a, b, c, d, e, f]   
 
 
-genId :: MicroTime -> NI.MAC -> U.Unique -> B.ByteString
+genId :: Integer -> NI.MAC -> U.Unique -> B.ByteString
 genId t m u =
-    let timestamp = toInteger t
-        shifted  = timestamp `shiftL` 32
+    let shifted  = t `shiftL` 32
         mac = toInteger $ (macToInt m) `shiftL` 16
         rruid = toInteger $ U.hashUnique u `mod` (2 `shiftL` 16)
         flake = shifted + mac + rruid
@@ -84,7 +57,7 @@ main = do
   where
     loop m s = forever $ do
         _ <- ZMQ.receive s
-        currentTime <- getMicroTime
+        currentTime <- msSinceEpoch
         unique <- U.newUnique
         let flake = genId currentTime m unique 
         ZMQ.send s [] flake
